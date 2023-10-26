d0196e6d5d6a9b5af437c2299fd41b3d4843331e
Carolyne <127044886+CarolSamoei@users.noreply.github.com>
2023-10-26 18:39:27 +0300

    Add files via upload

---
+++ Task 2 Prototype Documentation .py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # SDG Mapping Protype
+# 
+# ### BIOCLIMATIC AND ECOLOGICAL ZONES: KENYA
+# 
+# This document discusses two prototypes applied to two data sets on Bioclimatic and Ecological zones for mapping Sustainable 
+# Development Goals (SDGs). It will highlight the attributes used in mapping, the SDGs mapped and how they relate. 
+# 
+
+# ### Ecological Zones 
+# 
+# ##### Dataset Attribute – gez-name
+# This column represents different ecological zones including Tropical dry forest, tropical moist forest, tropical mountain system, tropical rainforest, Tropical shrubland and water. 
+# ##### SDGs Mapped
+# SDG 6- Clean Water and Sanitization: One of the ecological zones found in Kenya is Water or waterbodies. From the map, one of the visible water bodies is Lake Turkana. These water bodies provide clean water that is essential for human, life and animal life. 
+# SDG 13- Climate action: Presence of forested areas is critical in mitigating carbon in the atmosphere. Forests act as carbon sinks because it absorbs more carbon from the atmosphere than it releases. Water also acts as carbon sink thus promoting climate action. 
+# SDG 14- Life Under Water: Water bodies house life. They are ecological zones for plants and Animals. They therefore support marine life which can also provide food for humans. 
+# SDG 15- Life on Land: Ecological zones such as Tropical dry forest, tropical moist forest, tropical mountain system, tropical rainforest, Tropical shrubland are homes for any terrestrial life. Efforts directed towards protecting these areas promotes biodiversity. 
+# 
+
+# ### Agroecological Zones
+# 
+# ##### Dataset Attribute- value
+# This attribute represents different agroecological zones. They include Warm Temperate Moist, Warm Temperate Dry, Cool Temperate Moist, Cool Temperate Dry, Polar Moist, Polar Dry, Boreal Moist, Boreal Dry, Tropical Montane, Tropical Wet, Tropical Moist and Tropical Dry. 
+# ##### SDGs Mapped
+# SDG 2-Zero Hunger: Agroecological zones provide lie to different plants and crops. They ensure efforts towards sufficient, safe and nutritious food for all and reduce malnutrition levels. Achieving zero hunger is only possible if these zones are protected. 
+# SDG 12- Responsible Consumption and Production: Agroecological zones provides an avenue for agricultural decision and policies. Attaining sufficiency in food supply to support a growing population without compromising the ecosystem functioning is critical. Being aware of different agroecological zones and what crops they can support is key in ensuring the right agricultural approaches are used in food production thus supporting SDG 12.
+# SDG 13- Climate Action: Agricultural practices have a direct impact on climate. Agroecological practices like agroforestry provide buffers against droughts. They also prevent soil erosion and provide shelter to crops thus preventing heat waves. These zones can play these roles at different levels contributing to climate action efforts. 
+# SDG 15- Life on Land: Agroecological zones play a critical role in protecting biodiversity and conserving ecosystems. These ecosystems support life to human beings by providing sufficient oxygen, food, and energy.
+
+# ### References 
+# Akanmu, A. O., Akol, A. M., Ndolo, D. O., Kutu, F. R., & Babalola, O. O. (2023). Agroecological techniques: Adoption of safe and sustainable agricultural practices among the smallholder farmers in Africa. Frontiers in Sustainable Food Systems, 7, 1143061.
+# Weingärtner, L., Baum, D., Islam, J., Mishra, K., Nieskens, L., Bader, N., ... & Katsir, S. (2023). Advancing the Transformation of Agroecological Food Systems to Achieve Zero Hunger.
+# Balasubramanian, A. (2011/12/09)- CLASSIFYING THE BIOCLIMATIC ZONES. 10.13140/RG.2.2.32430.10562
+# SDGs Goals- https://sdgs.un.org/goals
